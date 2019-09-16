e59b0485a6520daf341605ad862c71d1fdbe3a2d
LightDiscord <root@arnaud.sh>
2019-09-16 21:25:28 +0200

    feat: remove useless tests and split main function into multiple

--- snake/src/lib.rs
+++ snake/src/lib.rs
@@ -9,11 +9,3 @@ pub use direction::Direction;
 pub use game::Game;
 pub use snake::Snake;
 pub use error::Error;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
--- web/src/main.rs
+++ web/src/main.rs
@@ -1,83 +1,63 @@
-use snake::Game;
-use snake::Direction;
+pub mod themes;
+pub mod draw;
+
+use snake::{ Game, Direction };
 
 use stdweb::traits::*;
 use stdweb::web::{ window, document, interval_buffered, CanvasRenderingContext2d };
 use stdweb::web::html_element::CanvasElement;
-use stdweb::web::event::ResizeEvent;
+use stdweb::web::event::{ ResizeEvent, KeyDownEvent };
 use stdweb::unstable::TryInto;
 
-use futures_util::stream::StreamExt;
-use failure::Fallible;
+use std::sync::{ Arc, RwLock };
 
-pub mod themes;
-pub mod draw;
+use futures_util::stream::StreamExt;
+use futures_util::future;
+
+use failure::Fallible;
 
 use draw::{ DrawContext, DrawLoop };
 
-use futures_util::future;
-use std::sync::{ Arc, RwLock };
+fn keydown_event(game: Arc<RwLock<Game>>) {
+    window().add_event_listener(move |event: KeyDownEvent| {
+        match event.key().as_str() {
+            "ArrowRight" => (game.write().unwrap().set_direction(Direction::Right)),
+            "ArrowLeft" => (game.write().unwrap().set_direction(Direction::Left)),
+            "ArrowUp" => (game.write().unwrap().set_direction(Direction::Up)),
+            "ArrowDown" => (game.write().unwrap().set_direction(Direction::Down)),
+            _ => ()
+        }
+    });
+}
+
+fn resize_canvas(canvas: &CanvasElement) {
+    canvas.set_width(canvas.offset_width() as u32);
+    canvas.set_height(canvas.offset_height() as u32);
+}
+
+fn resize_event(canvas: CanvasElement) {
+    window().add_event_listener(move |_: ResizeEvent| resize_canvas(&canvas));
+}
 
 fn main() -> Fallible<()> {
     let game = Arc::new(RwLock::new(Game::new()));
-    let canvas: CanvasElement = document()
-        .query_selector("#game")?
-        .unwrap()
-        .try_into()?;
-
+    let canvas: CanvasElement = document().query_selector("#game")?.unwrap().try_into()?;
     let context: CanvasRenderingContext2d = canvas.get_context()?;
 
-    canvas.set_width(canvas.offset_width() as u32);
-    canvas.set_height(canvas.offset_height() as u32);
+    resize_canvas(&canvas);
+    resize_event(canvas.clone());
+    keydown_event(game.clone());
 
-    window().add_event_listener({
-        let canvas = canvas.clone();
-        move |_: ResizeEvent| {
-            canvas.set_width(canvas.offset_width() as u32);
-            canvas.set_height(canvas.offset_height() as u32);
-        }
-    });
-
-    window().add_event_listener({
-        use stdweb::web::event::KeyDownEvent;
-
+    let tick_loop = interval_buffered(100).for_each({
         let game = game.clone();
-        move |event: KeyDownEvent| {
-            match event.key().as_str() {
-                "ArrowRight" => (game.write().unwrap().set_direction(Direction::Right)),
-                "ArrowLeft" => (game.write().unwrap().set_direction(Direction::Left)),
-                "ArrowUp" => (game.write().unwrap().set_direction(Direction::Up)),
-                "ArrowDown" => (game.write().unwrap().set_direction(Direction::Down)),
-                _ => ()
-            }
-        }
-    });
-
-    let render_game = game.clone();
-
-    let tick_loop = interval_buffered(100)
-        .for_each(move |_| {
+        move |_| {
             let _ = game.write().unwrap().tick();
             future::ready(())
-        });
+        }
+    });
 
     stdweb::spawn_local(tick_loop);
-
-    let draw_context = DrawContext {
-        canvas,
-        context,
-        game: render_game
-    };
-
-    DrawLoop::new(draw_context).start();
+    DrawLoop::new(DrawContext { canvas, context, game }).start();
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
